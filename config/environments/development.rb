19eb4ca7e09111d74c81e3ce1e51235a53760a59
soulchild <soulchildsince2013@gmail.com>
2016-02-07 01:44:01 +0800

    minor fix #4 duh
    moved Mechanizor into app/library and commented require/autoload stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Auto refresh file inside /lib folder
-  ActiveSupport::Dependencies.autoload_paths << File::join( Rails.root, 'lib')
-  ActiveSupport::Dependencies.explicitly_unloadable_constants << 'Mechanizor'
+  # ActiveSupport::Dependencies.autoload_paths << File::join( Rails.root, 'lib')
+  # ActiveSupport::Dependencies.explicitly_unloadable_constants << 'Mechanizor'
 end
--- config/initializers/requires.rb
+++
@@ -1,5 +0,0 @@
-# Require modules in lib
-
-Dir[File.join(Rails.root, 'lib', '*.rb')].each do |f|
-  require f
-end
