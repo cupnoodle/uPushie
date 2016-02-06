c4c36a868d9bf89baeab239174918dfacb0304d6
soulchild <soulchildsince2013@gmail.com>
2016-02-06 23:39:19 +0800

    Added student authentication API
    Added student#authenticate and Mechanizor’s login success check

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -23,15 +23,61 @@ module Api
       end
 
       # blank parameter lel
-      if params[:utar_id].blank? || params[:utar_password].blank? || !%w(ios iOS Android android).include?(params[:os])
+      if params[:utar_id].blank? || params[:utar_password].blank? || !%w(ios android).include?(params[:os].downcase)
         @result = {:message => 'Blank input or invalid operating system specified'}
-        render json: @result
+        render json: @result, :status => 400
         return
       end
 
-      @result = {message: Mechanizor.abc }
-      #respond_with(@result)
-      render :json => @result
+      # see if device_token/registration_id is provided for ios/android
+      if params[:os].downcase == 'ios' && !params.has_key?(:device_token)
+        @result = {:message => 'No device token supplied for ios'}
+        render json: @result, :status => 400
+        return
+      end
+
+      if params[:os].downcase == 'android' && !params.has_key?(:registration_id)
+        @result = {:message => 'No registration_id supplied for android'}
+        render json: @result, :status => 400
+        return
+      end
+        
+
+      ### Login to WBLE
+
+      if Mechanizor.login_success?(params[:utar_id], params[:utar_password])
+        @student = Student.find_by(:utar_id => params[:utar_id])
+
+        #found the user then update the os/device token/registration id
+        if @student
+          @student.os = params[:os].downcase
+          @student.last_login = Time.now
+
+        else
+          #student not found, create a new one
+          @student = Student.new(:utar_id => params[:utar_id], :device_token => params[:device_token], :last_login => Time.now)
+        end
+
+        # update os/device token/registration id accordingly
+        if(params[:os].downcase == 'ios')
+          @student.device_token = params[:device_token]
+          @student.registration_id = nil
+        end
+
+        if(params[:os].downcase == 'android')
+          @student.registration_id = params[:registration_id]
+          @student.device_token = nil
+        end
+
+        @student.save
+        @result = {message: 'Login successful for student ' + params[:utar_id] }
+        render :json => @result
+        return
+      end
+      
+      # login failed
+      @result = {message: 'Login failed for student ' + params[:utar_id] }
+      render :json => @result, :status => 403
 
     end
     
--- lib/mechanizor.rb
+++ lib/mechanizor.rb
@@ -1,8 +1,41 @@
 module Mechanizor
 
+  WBLE_BASE_URL = "https://wble-pk.utar.edu.my/"
+  WBLE_LOGIN_URL = WBLE_BASE_URL + "login/index.php"
   # Class function should add self. or {classname}. before method name
 
   def self.abc
     return "uhuhu"
   end
+
+  def self.login_success?(utar_id, utar_password)
+    page = Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE}.get WBLE_LOGIN_URL
+
+    #if wble is down, lulz
+    if page.code!='200'
+      return false
+    end
+
+    #Login to WBLE
+    form = page.forms.first
+    form['username'] = utar_id.to_s
+    form['password'] = utar_password.to_s
+    form['testcookies'] = '1'
+
+    page = form.submit
+
+    #if login failed , i.e detected the existence of login form or invalid status code
+    if page.code!='200' || page.at('.loginpanel')
+      return false
+    end
+
+    #no subject ,i.e wrong campus
+    if page.links_with(:text => /^[A-Z]{3,4}\d{4,5}/).count == 0
+      return false
+    end
+
+    return true
+
+  end
+  
 end
