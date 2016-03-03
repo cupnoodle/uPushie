0a6b35f1583118100f73448f271a61d0a7f55e3c
soulchild <soulchildsince2013@gmail.com>
2016-03-03 23:43:07 +0800

    Added campus enum to student/subject/subject_student models
    Added campus enum to student/subject/subject_student models

--- app/models/student.rb
+++ app/models/student.rb
@@ -12,6 +12,9 @@ class Student < ActiveRecord::Base
   # Add the enum set values to os, default is 0 (ios) as defined in the create_students.rb migration
   enum os: [:ios, :android]
 
+  # Add the enum set values to campus enum, 0 is perak, 1 is foundation perak, 2 is pj, 3 is foundation pj, 4 is kl, 5 is sungailong, 6 is sungailong-ipsr, 7 is fmhs
+  enum campus: [:pk, :cfspk, :pj, :cfspj, :kl, :sl, :ipsrsl, :fmhs]
+
   # validate presence of device_token if os is ios
   # validates_presence_of :device_token, :if => lambda { self.ios? }
 
